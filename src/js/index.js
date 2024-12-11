0feb3562de4bd2b9c5b71d3c0fdf64c06a794e0d
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-11 20:33:05 +0200

    added cityFeelsLike, cityName, weatheIcon, citySunrise and citySunset to getCityData()

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
+import '../css/style.css';
+
 const weatherApp = (function () {
-  console.log('Working');
+  //get city name from user and use it in the api
+  const getCityData = async (cityName) => {
+    const res = await fetch(
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidelber%20South%20Africa?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json'
+    );
+    const resWeather = res.json().then((res) => {
+      const cityConditions = res.currentConditions.conditions;
+      const cityFeelsLike = res.currentConditions.feelslike;
+      const cityName = res.resolvedAddress;
+      const weatherIcon = res.currentConditions.icon;
+      const citySunrise = res.currentConditions.sunrise;
+      const citySunset = res.currentConditions.sunset;
+
+      console.log(res);
+    });
+  };
+
+  //declare functions down here:
+  getCityData().then((res) => {});
 })();
