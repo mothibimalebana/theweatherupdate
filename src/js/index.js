11113f6feeb70e90770e63a4caafb91cb0d94a12
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-12 13:17:58 +0200

    getResponse returns object

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,18 @@ import '../css/style.css';
 
 const weatherApp = (function () {
   //get city name from user and use it in the api
-  const getCityData = async (cityName) => {
+  const getResponse = async () => {
     const res = await fetch(
-      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidelber%20South%20Africa?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json'
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidelber%20South%20Africa?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json',
+      { mode: 'cors' }
     );
-    const resWeather = res.json().then((res) => {
-      const cityConditions = res.currentConditions.conditions;
-      const cityFeelsLike = res.currentConditions.feelslike;
-      const cityName = res.resolvedAddress;
-      const weatherIcon = res.currentConditions.icon;
-      const citySunrise = res.currentConditions.sunrise;
-      const citySunset = res.currentConditions.sunset;
-
-      console.log(res);
-    });
+    return res.json();
   };
+  const getCity = getResponse().then((response) => {
+    const cityConditions = response.currentConditions.conditions;
+    console.log(cityConditions);
+  });
 
   //declare functions down here:
-  getCityData().then((res) => {});
+  console.log(getResponse());
 })();
