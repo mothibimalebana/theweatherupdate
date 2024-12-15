65148f06479cde095f30b9a58cc6bb4794a15bbc
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-15 07:13:38 +0200

    if user does not enter a city name, Heidelberg will be the default

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,23 @@ import '../css/style.css';
 
 const weatherApp = (function () {
   //get city name from user and use it in the api
-  const getResponse = async () => {
-    const res = await fetch(
-      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidelber%20South%20Africa?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json',
-      { mode: 'cors' }
-    );
-    return res.json();
+  const getResponse = async (cityName) => {
+    //if user does not enter city name, default to my city : )
+    if (cityName == '') {
+      const res = await fetch(
+        'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidelber%20South%20Africa?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json',
+        { mode: 'cors' }
+      );
+      return res.json();
+    } else {
+      const res = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json`,
+        { mode: 'cors' }
+      );
+      return res.json();
+    }
   };
-  const getData = getResponse().then((response) => {
+  const getCityData = getResponse().then((response) => {
     const citySunrise = response.currentConditions.sunrise;
     const citySunset = response.currentConditions.sunset;
     const cityConditions = response.currentConditions.conditions;
@@ -30,7 +39,6 @@ const weatherApp = (function () {
     return cityData;
   });
 
-  const getCityData = getData;
   //declare functions down here:
   console.log(getCityData);
 })();
