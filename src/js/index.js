509c2c955abf7337246bc1f16d1bb90b139c2a13
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-15 08:37:16 +0200

    getCity now takes parameter values(city names) and returns responses based on those values

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import '../css/style.css';
 
 const weatherApp = (function () {
   //get city name from user and use it in the api
-  const getResponse = async (cityName) => {
+  let cityName;
+
+  const getCity = async function cityResponse(city) {
     //if user does not enter city name, default to my city : )
-    if (cityName == '') {
+    if (city == undefined) {
       const res = await fetch(
         'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidelber%20South%20Africa?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json',
         { mode: 'cors' }
@@ -12,13 +14,13 @@ const weatherApp = (function () {
       return res.json();
     } else {
       const res = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json`,
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json`,
         { mode: 'cors' }
       );
       return res.json();
     }
   };
-  const getCityData = getResponse().then((response) => {
+  const getCityData = getCity().then((response) => {
     const citySunrise = response.currentConditions.sunrise;
     const citySunset = response.currentConditions.sunset;
     const cityConditions = response.currentConditions.conditions;
@@ -40,5 +42,6 @@ const weatherApp = (function () {
   });
 
   //declare functions down here:
+  console.log(getCity('Midrand'));
   console.log(getCityData);
 })();
