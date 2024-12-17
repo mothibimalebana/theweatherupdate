593c1977ed9465efe591441520766f7bea75291a
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-17 19:55:47 +0200

    background displays from giphy api

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 import '../css/style.css';
 
 const weatherApp = (function () {
-  //get city name from user and use it in the api
-  let cityName;
-
-  const getCity = async function cityResponse(city) {
-    //if user does not enter city name, default to my city : )
+  const container = document.querySelector('.container');
+  container;
+  const getCityResponse = async (city) => {
     if (city == undefined) {
       const res = await fetch(
         'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Heidelber%20South%20Africa?unitGroup=metric&include=current&key=6W3RPBWACVQN6E7X2BYT43F9H&contentType=json',
@@ -20,28 +18,38 @@ const weatherApp = (function () {
       return res.json();
     }
   };
-  const getCityData = getCity().then((response) => {
-    const citySunrise = response.currentConditions.sunrise;
-    const citySunset = response.currentConditions.sunset;
-    const cityConditions = response.currentConditions.conditions;
-    const cityFeelsLike = response.currentConditions.feelslike;
-    const cityIcon = response.currentConditions.icon;
-    const cityTemp = response.currentConditions.temp;
-    const address = response.resolvedAddress;
+  const getWeatherGIF = async (weatherIcon) => {
+    const res = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=oqVqtKYI8bdtGH4vFdngKiJ7GGC0wX2I&s=${weatherIcon}&weirdness=4`,
+      { mode: 'cors' }
+    );
+    console.log(res.json());
+  };
+  getWeatherGIF();
+  const getCityData = (city) => {
+    const getCityData = getCityResponse(city).then((response) => {
+      const citySunrise = response.currentConditions.sunrise;
+      const citySunset = response.currentConditions.sunset;
+      const cityConditions = response.currentConditions.conditions;
+      const cityFeelsLike = response.currentConditions.feelslike;
+      const cityIcon = response.currentConditions.icon;
+      const cityTemp = response.currentConditions.temp;
+      const address = response.resolvedAddress;
 
-    const cityData = {
-      citySunrise,
-      citySunset,
-      cityConditions,
-      cityFeelsLike,
-      cityIcon,
-      cityTemp,
-      address,
-    };
-    return cityData;
-  });
+      const cityData = {
+        citySunrise,
+        citySunset,
+        cityConditions,
+        cityFeelsLike,
+        cityIcon,
+        cityTemp,
+        address,
+      };
+      console.log(cityData);
+      return cityData;
+    });
+  };
 
   //declare functions down here:
-  console.log(getCity('Midrand'));
-  console.log(getCityData);
+  console.log(getCityData('midrand'));
 })();
